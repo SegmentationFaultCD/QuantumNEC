131eb7b65e639e6ca1d2d32e9c76da87f5dd6c49
SegmentationFaultCD <3076253106@qq.com>
2026-01-03 09:38:00 +0800

    None

--- test/1.cpp
+++ test/1.cpp
@@ -58,154 +58,22 @@ struct IPC {
     auto send( std::uint64_t destination, auto &&...args ) {
     }
 };
-
-#include <concepts>
 #include <queue>
-#include <ranges>
-#include <utility>
 #include <vector>
-template < typename D >
-struct root_mode {
-    root_mode( ) = default;
 
-    auto operator( )( std::strong_ordering ord ) const {
-        return D { }.compare( ord );
+struct task {
+    std::jthread core;
+    std::uint64_t vd;
+    std::uint64_t prio;
+};
+class task_cmp {
+public:
+    bool operator( )( const task &__x, const task &__y ) const {
+        return __x.vd > __y.vd;
     }
 };
 
-class Rmax : public root_mode< Rmax > {
-    friend root_mode;
-
-public:
-    Rmax( ) = default;
-
-private:
-    auto compare( std::strong_ordering ord ) {
-        return ord == std::strong_ordering::greater;
-    }
-
-} rmax;
-class Rmin : public root_mode< Rmin > {
-    friend root_mode;
-
-public:
-    Rmin( ) = default;
-
-private:
-    auto compare( std::strong_ordering ord ) {
-        return ord == std::strong_ordering::less;
-    }
-} rmin;
-
-template < typename T, root_mode r = rmin >
-    requires std::totally_ordered< T > && std::movable< T >
-struct priority_queue {
-    auto push( T &&x ) {
-        heap.push_back( x );
-        size++;
-        up( size );
-    }
-    auto pop( void ) -> T {
-        down( 1 );
-        auto element = *heap.end( );
-        heap.pop_back( );
-        return element;
-    }
-
-    auto up( std::uint64_t x ) {
-        while ( x > 1 && r( heap[ x ] <=> heap[ x / 2 ] ) ) {
-            std::swap( heap[ x ], heap[ x / 2 ] );
-            x /= 2;
-        }
-    }
-    auto down( std::uint64_t x ) {
-        std::uint64_t t;
-        while ( x * 2 <= size ) {
-            t = x * 2;
-            if ( t + 1 <= size && !r( heap[ t + 1 ] <=> heap[ t ] ) )
-                t++;
-            if ( heap[ t ] <= heap[ x ] )
-                break;
-            std::swap( heap[ x ], heap[ t ] );
-            x = t;
-        }
-    }
-
-public:
-    std::vector< T > heap { T {} };     // 第一个留空
-    std::uint64_t size = 0;
-};
-struct ScheduleData {
-    std::uint64_t time_slice;
-    std::uint64_t priority;
-    std::uint64_t nice;
-    std::uint64_t virtual_deadline;
-    std::uint64_t cpu;
-};
-struct Task {
-public:
-    std::thread self;
-    ScheduleData data;
-    Task( auto &&lambda, auto &&...args ) :
-        self { lambda, std::forward< decltype( auto { args } ) >( args )... } {
-    }
-
-    Task( Task && ) = default;
-
-    Task( ) = default;
-
-    auto operator<=>( const Task &oper ) const {
-        return this->data.virtual_deadline <=> oper.data.virtual_deadline;
-    }
-    auto operator==( const Task &oper ) const {
-        return this->data.virtual_deadline == oper.data.virtual_deadline;
-    }
-    template < typename U >
-        requires std::convertible_to< U, Task >
-    auto operator=( this auto &&self, U &&o ) -> Task & {
-        std::thread s { std::move( self.self ) };
-        self.self = std::move( o.self );
-        o.self = std::move( s );
-        std::swap( self.data, o.data );
-        return *self;
-    }
-} task[ 4 ];
-auto operator<=>( Task &A, Task &oper ) -> std::strong_ordering {
-    return A.data.virtual_deadline <=> oper.data.virtual_deadline;
-}
-// namespace std
-class Muqss {
-    constexpr static double min_prio_ratio = 1.0;     // 静态优先级在时间片计算的权重
-    constexpr static auto rr_interval = 6ul;          // 6ms,这个一般作为时间片填充
-    // nice默认为0，如要更改使用系统调用, 更改优先级，重新计算VD
-    constexpr static auto default_nice = 0;
-    // nice有40个
-    constexpr static auto min_nice = -20;
-    constexpr static auto max_nice = 40;
-    auto get_prio_ratio( std::uint64_t nice ) {
-        auto prio_ratio = this->min_prio_ratio;
-        for ( auto i = 1; i <= nice - this->min_nice; ++i ) {
-            prio_ratio *= 1.1;
-        }
-        return prio_ratio;
-    }
-
-    // VD(Virtual Deadline) 计算公式为 niffies(纳秒级最小时间间隔计数) + (prio_ratio * rr_interval)
-
-    auto get_virtual_deadline( std::uint64_t now_time, double prio_ratio ) {
-        return now_time + prio_ratio * this->rr_interval;
-    }
-
-    std::queue< Task > RT_task;
-    std::priority_queue< int, std::vector< int >, std::greater< int > > queue;
-};
+std::priority_queue< task, std::vector< task >, task_cmp > s;
 
 auto main( void ) -> int {
-    int i = 0;
-    for ( auto thd : std::ranges::views::repeat( []( int id ) {
-                         std::println( "{}", id );
-                     } ) | std::ranges::views::take( 4 ) ) {
-        std::construct_at( &task[ i ], thd, i );
-        ++i;
-    }
 }
